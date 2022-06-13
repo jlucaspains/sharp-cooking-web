7c5c80a5c832fafb8f1c1010f3127e32d93ad372
Lucas Pains <lpains@jbssa.com>
2022-06-13 11:55:22 -0600

    Added option buttons

--- src/pages/recipe/[id]/index.vue
+++ src/pages/recipe/[id]/index.vue
@@ -97,16 +97,95 @@ function editItem() {
         <polyline points="21 15 16 10 5 21" />
       </svg>
     </div>
-    <div class="w-full">
+    <div class="float-right">
       <button
-        class="p-2 w-10 h-10 m-2 bg-theme-primary rounded-full hover:bg-theme-secondary focus:ring-4 focus:ring-theme-primary focus:outline-none shadow-lg"
+        class="w-12 h-12 m-1 bg-theme-primary rounded-full hover:bg-theme-secondary focus:ring-4 focus:ring-theme-primary focus:outline-none shadow-lg"
         @click="editItem"
       >
-        <svg class="h-5 w-5 text-white" width="24"  height="24"  viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" /></svg>
+        <svg
+          class="h-5 w-5 text-white m-auto"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
+        </svg>
       </button>
-      <button>action 2</button>
-      <button>action 3</button>
+      <button
+        class="w-12 h-12 m-1 bg-theme-primary rounded-full hover:bg-theme-secondary focus:ring-4 focus:ring-theme-primary focus:outline-none shadow-lg"
+        @click="toggleScreenLight"
+      >
+        <svg
+          class="h-5 w-5 text-white m-auto"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+          />
+        </svg>
+      </button>
+      <button
+        class="w-12 h-12 m-1 p-0 bg-theme-primary rounded-full hover:bg-theme-secondary focus:ring-4 focus:ring-theme-primary focus:outline-none shadow-lg"
+        @click="changeServings"
+      >
+        <span class="text-white text-sm m-auto my-0 py-0">1x</span>
+      </button>
+      <button
+        class="w-12 h-12 m-1 bg-theme-primary rounded-full hover:bg-theme-secondary focus:ring-4 focus:ring-theme-primary focus:outline-none shadow-lg"
+        @click="changeTime"
+      >
+        <svg
+          class="h-5 w-5 text-white m-auto"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" />
+          <circle cx="12" cy="12" r="9" />
+          <polyline points="12 7 12 12 15 15" />
+        </svg>
+      </button>
+      <button
+        class="w-12 h-12 m-1 bg-theme-primary rounded-full hover:bg-theme-secondary focus:ring-4 focus:ring-theme-primary focus:outline-none shadow-lg"
+        @click="printItem"
+      >
+        <svg
+          class="h-5 w-5 text-white m-auto"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" />
+          <path
+            d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2"
+          />
+          <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
+          <rect x="7" y="13" width="10" height="8" rx="2" />
+        </svg>
+      </button>
     </div>
+
     <div class="grid grid-cols-8 w-full mt-7">
       <template v-for="displayItem in display">
         <div class="col-span-2 mt-3">{{ displayItem.time }}</div>
