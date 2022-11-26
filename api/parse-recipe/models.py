94ea355bca1af1f7f3e2e45a393f8fb19c03b37e
lpains <jlucaspains@gmail.com>
2022-11-25 18:14:01 -0700

    Added first API

---
+++ api/parse-recipe/models.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+class BaseModel():
+    test: str
+
+class RecipeIngredient(BaseModel):
+    raw: str
+    quantity: float
+    unit: str
+
+class RecipeInstruction(BaseModel):
+    raw: str
+    minutes: float
+
+class Recipe(BaseModel):
+    title: Union[str, None] = None
+    totalTime: Union[int, None] = None
+    yields: Union[str, None] = None
+    ingredients: list[RecipeIngredient] = []
+    instructions: list[RecipeInstruction] = []
+    image: Union[str, None] = None
+    host: Union[str, None] = None
+    notes: Union[str, None] = None
+
+class ParseRequest(BaseModel):
+    url: str
+    downloadImage: bool = False
+
+class ImageResult(BaseModel):
+    name: str
+    image: str
