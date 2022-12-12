0ed0d4a3bc0738f7c96aaa957d4749a59f3aafab
lpains <jlucaspains@gmail.com>
2022-12-11 20:18:26 -0700

    regex are now in translations

--- src/services/recipe.ts
+++ src/services/recipe.ts
@@ -1,9 +1,19 @@
+export class Ingredient {
+    constructor(public quantity: string, public uom: string, public content: string) { }
+}
+
+export class Step {
+    constructor(public time: number, public content: string) { }
+}
+
 export class Recipe {
     id?: number;
     title!: string;
     score!: number;
     ingredients!: string[];
+    // parsedIngredients?: Ingredient[];
     steps!: string[];
+    // parsedSteps?: Step[];
     notes!: string;
     multiplier: number = 1;
     changedOn!: string;
