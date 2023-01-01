0ff0495b225940cf625d6454ca39a157fc907e70
jlucaspains <jlucaspains@gmail.com>
2023-01-01 14:44:06 -0700

    Adjusted configs and added yarn to CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,11 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html'],
+    ['junit', { outputFile: './test-results/junit.xml' }],    
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -40,6 +44,8 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'off',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
