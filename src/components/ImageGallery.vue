3b5cfc687301657425c7ac4d3d86ca5b5ab3ba00
jlucaspains <jlucaspains@gmail.com>
2024-02-02 19:04:50 -0700

    One more...

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -128,8 +128,8 @@ function close() {
                                     :class="{ 'm-auto max-w-full': true, 'lg:max-h-80 max-h-60': !isOpen, 'lg:max-h-full': isOpen }">
                             </a>
                             <object v-else>
-                                <param name="movie" :value="image.url + '?enablejsapi=1&fs=1&origin=' + originUrl" />
-                                <embed :src="image.url + '?enablejsapi=1&fs=1&origin=' + originUrl"
+                                <param name="movie" :value="'https://www.youtube.com/watch?v=GS8jz_ta9vs'" />
+                                <embed :src="'https://www.youtube.com/watch?v=GS8jz_ta9vs'"
                                     type="application/x-shockwave-flash" />
                             </object>
                             <!-- <iframe  title="Youtube video" class="lg:h-80 h-60" type="text/html" width="100%"
