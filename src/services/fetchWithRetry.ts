4461c855818f26e734599a2cb3a5da51aea47711
jlucaspains <jlucaspains@gmail.com>
2024-02-10 20:51:04 -0700

    Only retry on certain http codes.

--- src/services/fetchWithRetry.ts
+++ src/services/fetchWithRetry.ts
@@ -1,13 +1,10 @@
 export async function fetchWithRetry(url: RequestInfo | URL, options?: RequestInit | undefined, retries: number = 0): Promise<Response> {
     const res = await fetch(url, options);
+    const statusesToRetry = [408, 429, 500, 502, 503, 504];
 
-    if (res.ok) {
+    if (res.ok || retries == 0 || !statusesToRetry.includes(res.status)) {
         return res;
     }
 
-    if (retries > 0) {
-        return fetchWithRetry(url, options, retries - 1)
-    } else {
-        return res;
-    }
+    return fetchWithRetry(url, options, retries - 1)
 }
