565e5bb99b8486fa24b5044a2bbd3972d097a293
jlucaspains <jlucaspains@gmail.com>
2024-07-15 20:48:30 -0600

    Fixed warning

--- src/pages/recipe/[id]/index.vue
+++ src/pages/recipe/[id]/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, computed, nextTick } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import {
   getRecipe,
