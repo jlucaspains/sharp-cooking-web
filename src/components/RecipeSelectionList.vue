59f8a8b02efe944ec5fc507f07a3a1f0dbede93e
Lucas Pains <jlucaspains@gmail.com>
2026-01-24 17:12:05 -0700

    More styling adjustments

--- src/components/RecipeSelectionList.vue
+++ src/components/RecipeSelectionList.vue
@@ -81,9 +81,6 @@ function handleKeydown(event: KeyboardEvent, recipeId: number) {
         />
         <div class="flex-1">
           <h3 class="font-medium dark:text-white text-gray-900">{{ recipe.title }}</h3>
-          <p v-if="recipe.categoryId" class="text-sm text-gray-500">
-            Category ID: {{ recipe.categoryId }}
-          </p>
         </div>
       </div>
 
