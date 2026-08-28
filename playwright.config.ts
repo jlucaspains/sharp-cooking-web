99f17b10ed10bb2ca320f299f6c832cd1109d092
Lucas Pains <jlucaspains@gmail.com>
2026-08-28 12:26:53 -0300

    Restore CI specific test configuration and removed sonar cloud

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,10 +26,9 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  /* retries: process.env.CI ? 2 : 0, */
-  retries: 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  /* workers: process.env.CI ? 2 : undefined,*/
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
